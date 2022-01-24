04fdc37e8f419f57983ac583122f112545c323e5
Sai Pavan <32958525+saipavan10-git@users.noreply.github.com>
2022-01-23 20:46:58 -0500

    Add files via upload

---
+++ src/test/PasswordManagementSysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PasswordManagementSys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PasswordManagementSysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
